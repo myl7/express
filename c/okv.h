250b768bb8c7b5a90bcddcadca3cb45e6a94faf1
myl7 <myl@myl.moe>
2024-08-15 10:46:03 +0800

    Add eval code

--- c/okv.h
+++ c/okv.h
@@ -3,13 +3,13 @@
 
 #include "dpf.h"
 
-typedef struct{
-	uint128_t rowID; //128 bit virtual address
-    //uint128_t rowKey; //key used to encrypt for rerandomizing
-    EVP_CIPHER_CTX *rowKey;
-    int dataSize; //size of the data stored here
-    uint8_t* mask; //current mask resulting from rerandomization
-    uint8_t* data; //the actual data
+typedef struct {
+  uint128_t rowID;  // 128 bit virtual address
+  // uint128_t rowKey; //key used to encrypt for rerandomizing
+  EVP_CIPHER_CTX *rowKey;
+  int dataSize;   // size of the data stored here
+  uint8_t *mask;  // current mask resulting from rerandomization
+  uint8_t *data;  // the actual data
 } vatRow;
 
 int initializeServer(int numThreads);
